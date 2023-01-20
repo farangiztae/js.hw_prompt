8910dc2e27a1fa337f5805a5459388419f253fb8
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2023-01-20 16:54:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let object = {
       name:{},
       surname:{},
-      age:{}
+      age:{},
+      colour:{}
 };
 
 let first = prompt("Ismingiz nma?");
 let second = prompt("Familiyangiz nma?");
 let third = prompt("Yoshingiz nechida?");
+let fourth = prompt("Yaxshi kurgan rangingiz?")
 
 object.name[first] = first;
 object.surname[second] = second;
 object.age[third] = third;
+object.colour[fourth] = fourth;
 
 console.log(object);
